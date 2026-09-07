2d124913fda24169000d841ae2a0d462e1a3b4cc
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-07 22:49:45 +0530

    update Test.js

---
+++ src/gas/Test.js
@@ -0,0 +1,12 @@
+function testAppointment() {
+  // Tab to edit
+  bookAppointment({
+    userName: "Himaghna Dasgupta",
+      userEmail: "himaghna.dasgupta@gmail.com",
+      userPhone: "8017391410",
+      doctorName: "Dr. Smith",
+      doctorEmail: "test@doctors.com",
+      date: "2026-09-07",
+      time: "09:00"
+  })
+}
