ebf4fdcdddd5b251f48a42f99fe426d0368eb6cf
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-08 16:36:36 +0530

    update SignupView.jsx

--- src/views/SignupView.jsx
+++ src/views/SignupView.jsx
@@ -42,12 +42,12 @@ export default function SignupView({ onSubmit, onNavigateLogin, styles }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(isEmailVerified)
+    alert(isEmailVerified);
     if (!isEmailVerified) {
       setOtpStatus('Please verify your email address first.');
       return;
     }
-    alert(isPasswordValid)
+    alert(isPasswordValid);
     if (!isPasswordValid) return;
     alert("submitting...");
     onSubmit(formData);
