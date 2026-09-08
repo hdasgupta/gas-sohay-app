e8f3725322cf40b4fc304315401602858d17ee7d
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-08 09:05:23 +0530

    update backend.js

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -1,7 +1,7 @@
 export const callBackend = (fn, args = [], cb) => {
   if (typeof google !== 'undefined' && google.script) {
     google.script.run
-    .withFailureHandler((e) => alert(JSON.stringify(e))
+    .withFailureHandler((e) => alert(JSON.stringify(e))) 
     .withSuccessHandler(cb)[fn](...args);
   } else {
     // Development Local Mocking
