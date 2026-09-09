124f5b03ac555a700356083d7d905b07c85880be
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-10 01:42:06 +0530

    update Test.js

--- src/gas/Test.js
+++ src/gas/Test.js
@@ -1,14 +1,13 @@
 function testAppointment() {
   // Tab to edit
-  bookAppointment({
-    userName: "Himaghna Dasgupta",
-      userEmail: "himaghna.dasgupta@gmail.com",
-      userPhone: "8017391410",
-      doctorName: "Dr. Smith",
-      doctorEmail: "test@doctors.com",
-      date: "\"2026-09-07\"",
-      time: "\"09:00\""
-  })
+  bookAppointment({ 
+  patientEmail: "himaghna.dasgupta@gmail.com",
+  patientName: "Himaghna Dasgupta",
+  bookedByEmail: "himaghna.dasgupta@gmail.com",
+  doctorEmail:"test@gmail.com",
+  date: JSON.stringify("2026-09-10"),
+  time: JSON.strungify("09:00")
+})
 }
 
 function testCreatePrescription() {
