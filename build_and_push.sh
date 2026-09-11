4d7a43bdc2ca098015f4fe8e5af0a5bbd50a8fa5
Himaghna Dasgupta <himaghna.dasgupta@gmail.com>
2026-09-11 21:38:40 +0530

    Update build shell script

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,5 +2,7 @@ git fetch
 git merge origin/main
 npm install
 npm run build
+sh find_duplicate_gas_functions.sh ./src/gas
+sh find_undeclared.sh ./src/gas/
 sh copy_js_to_gs.sh ./src/gas/ ./dist/
 clasp push
