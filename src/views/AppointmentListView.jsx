167f467eb5dc2e2a3f630f72a13aead61fea6c2f
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-11 10:10:10 +0530

    update AppointmentListView.jsx

--- src/views/AppointmentListView.jsx
+++ src/views/AppointmentListView.jsx
@@ -28,6 +28,7 @@ export default function AppointmentListView({ user, styles = {} }) {
     setLoading(true);
     callBackend('getAppointmentsForUser', [user?.email || ''], (data) => {
       setLoading(false);
+      alert(JSON.stringify(data))
       setAppointments(data || []);
     });
   };
