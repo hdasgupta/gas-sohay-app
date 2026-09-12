08eac44091f6337086a105c606df4bc42156b208
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-12 21:21:10 +0530

    update AdminView.jsx

--- src/views/AdminView.jsx
+++ src/views/AdminView.jsx
@@ -566,7 +566,7 @@ export default function AdminView({ styles = {} }) {
                   </div>
                 </div>
               </div>
-            )}
+            ))}
           </div>
         )} 
       </div>
