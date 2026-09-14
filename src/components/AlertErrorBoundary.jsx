4fe245ef766252fbce042228b4c5a8ec0f10d0a9
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-14 12:45:19 +0530

    update AlertErrorBoundary.jsx

--- src/components/AlertErrorBoundary.jsx
+++ src/components/AlertErrorBoundary.jsx
@@ -12,7 +12,7 @@ export default class AlertErrorBoundary extends React.Component {
 
   componentDidCatch(error, errorInfo) {
     // Triggers native browser alert on runtime error
-    alert(`Runtime Error: ${error.message}`);
+    alert(`Runtime Error: ${JSON.stringify(error)}, ${JSON.stringify(errorInfo)}`);
   }
 
   render() {
