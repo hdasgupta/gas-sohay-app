1117dd387bf38c70187cd7105f39a7cc4afe1642
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-14 12:30:22 +0530

    update AlertErrorBoundary.jsx

--- src/components/AlertErrorBoundary.jsx
+++ src/components/AlertErrorBoundary.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class AlertErrorBoundary extends React.Component {
+export default class AlertErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
