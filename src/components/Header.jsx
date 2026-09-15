e3c8e824f0d3137b5da24fe7de04e705b224f60a
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-15 13:21:02 +0530

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 export default function Header({ logo, user, orgName, onLogout, styles }) {
   if (!user) return null;
   return (
+    <>
     <div style={styles.header}>
       <span>Logged in: <strong>{user.name}</strong>[{user.email}] ({user.role})</span>
       <button onClick={onLogout} style={styles.btnDanger}>Logout</button>
-      <div>
+      
+    </div>
+    <div>
         <img 
           src={logo} 
           alt={`${orgName} Logo`} 
           className="header-logo" 
+          height="300"
+          width="300"
         />
         <h1>{orgName}</h1>
       </div>
-    </div>
+      </>
   );
 }
