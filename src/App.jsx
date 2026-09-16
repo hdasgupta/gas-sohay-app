8fce331dd58117e5191eea42669f704204a5850e
hdasgupta <himaghna.dasgupta@gmail.com>
2026-09-16 10:52:42 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -171,11 +171,11 @@ export default function App() {
        {confirmation && (
         <MeetLinkCard key="confirmation" confirmation={confirmation} user={user} onReset={() => setView('appointments')} styles={styles} />
       )}
-      <ConferenceView 
+      {window.roomid && <ConferenceView 
         key="conference"
         user={user}
         appointmentId={window.roomId}
-      />
+      />}
       </SlideView>
     </div>
     
